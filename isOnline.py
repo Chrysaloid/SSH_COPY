e2393d47b2a2abc03a549b72e64c9f2a88e300bf
Chrysaloid <58228915+Chrysaloid@users.noreply.github.com>
2025-08-27 23:47:52 +0200

    Delete isOnline.py as it is no longer needed

--- isOnline.py
+++
@@ -1,7 +0,0 @@
-from pythonping import ping
-
-def isOnline(ip: str, timeout = 0.5) -> bool:
-	try:
-		return ping(ip, timeout=timeout, count=1, size=32).success()
-	except Exception:
-		return False
